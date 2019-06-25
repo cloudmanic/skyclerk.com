691bc1e6091f28a0ca63f807fbc156d717d81171
Spicer Matthews <github@cloudmanic.com>
2019-06-25 06:54:36 -0700

    Got contact form working.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@
 	window.app = app;
 
 	app.init = function() {
-		//this.contactUs();
+		this.contactUs();
 		this.supportSearch();
 		this.newsletterSubscribe();
 	};
@@ -13,67 +13,65 @@
 	 * Contact us page
 	 *
 	 */
-	// app.contactUs = function() {
-	// 	$('.contact-us .submit').on('submit', function(event) {
-	// 		event.preventDefault();
-	//
-	// 		var $this = $(this);
-	// 		var error = false;
-	// 		var email = $this.find("#email").val();
-	// 		var subject = $this.find("#subject").val();
-	// 		var body = $this.find("#body").val();
-	//
-	// 		if(email.length <= 0) {
-	// 			error = true;
-	// 			$this.find("#email").parent().addClass('has-error');
-	// 			$this.find("#email").parent().find("p").html("Email address is required.");
-	// 		} else {
-	// 			$this.find("#email").parent().removeClass('has-error');
-	// 			$this.find("#email").parent().find("p").html("");
-	// 		}
-	//
-	// 		if(subject.length <= 0) {
-	// 			error = true;
-	// 			$this.find("#subject").parent().addClass('has-error');
-	// 			$this.find("#subject").parent().find("p").html("Subject is required.");
-	// 		} else {
-	// 			$this.find("#subject").parent().removeClass('has-error');
-	// 			$this.find("#subject").parent().find("p").html("");
-	// 		}
-	//
-	// 		if(body.length <= 0) {
-	// 			error = true;
-	// 			$this.find("#body").parent().addClass('has-error');
-	// 			$this.find("#body").parent().find("p").html("Body is required.");
-	// 		} else {
-	// 			$this.find("#body").parent().removeClass('has-error');
-	// 			$this.find("#body").parent().find("p").html("");
-	// 		}
-	//
-	// 		// If we have error do not continue.
-	// 		if(error) {
-	// 			return false;
-	// 		}
-	//
-	// 		// Send it to the server
-	//     $.post({
-	//         url: '/',
-	//         dataType: 'json',
-	//         data: $this.serialize(),
-	//         success: function(response) {
-	//           if (response.success) {
-	// 						$this.hide();
-	// 						$('.help-page .section__title').hide();
-	// 						$('.contact-us .success').fadeIn();
-	//           } else {
-	// 						// response.error will be an object containing any validation errors that occurred, indexed by field name
-	// 						// e.g. response.error.fromName => ['From Name is required']
-	// 						alert('An error occurred. Please try again.');
-	//           }
-	//         }
-	//     });
-	// 	});
-	// };
+	app.contactUs = function() {
+		$('.contact-us .submit').on('submit', function(event) {
+			event.preventDefault();
+
+			var $this = $(this);
+			var error = false;
+			var email = $this.find("#email").val();
+			var subject = $this.find("#subject").val();
+			var body = $this.find("#body").val();
+
+			if(email.length <= 0) {
+				error = true;
+				$this.find("#email").parent().parent().addClass('with-error');
+			} else {
+				$this.find("#email").parent().parent().removeClass('with-error');
+			}
+
+			if(subject.length <= 0) {
+				error = true;
+				$this.find("#subject").parent().parent().addClass('with-error');
+			} else {
+				$this.find("#subject").parent().parent().removeClass('with-error');
+			}
+
+			if(body.length <= 0) {
+				error = true;
+				$this.find("#body").parent().parent().addClass('with-error');
+			} else {
+				$this.find("#body").parent().parent().removeClass('with-error');
+			}
+
+			// If we have error do not continue.
+			if(error) {
+				return false;
+			}
+
+			// Change button.
+			$('#submit-btn').text("Sending...");
+
+			// Send it to the server
+	    $.post({
+	        url: '/',
+	        dataType: 'json',
+	        data: $this.serialize(),
+	        success: function(response) {
+	          if (response.success) {
+							$this.hide();
+							$('.form__head').hide();
+							$('.contact-us .success').fadeIn();
+	          } else {
+							$('#submit-btn').text("Submit Request");
+							// response.error will be an object containing any validation errors that occurred, indexed by field name
+							// e.g. response.error.fromName => ['From Name is required']
+							alert('An error occurred. Please try again.');
+	          }
+	        }
+	    });
+		});
+	};
 
 	/**
 	 * Newsletter subscribe
