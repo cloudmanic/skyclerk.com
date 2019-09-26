a8b54fbf8bbbef1525adf61524401044272f19cb
Spicer Matthews <github@cloudmanic.com>
2019-09-26 13:33:27 -0700

    More URL struff

--- modules/cloudmanic-craft-nginx/controllers/Cache.php
+++ modules/cloudmanic-craft-nginx/controllers/Cache.php
@@ -23,10 +23,10 @@ class CacheController extends Controller
     }
 
     exec("rm -rf /tmp/nginx/*");
-    
+
     // Rebuild cache
     $curl = curl_init();
-    
+
     curl_setopt_array($curl, [
       CURLOPT_URL => getenv('NGINX_CACHE_WARM_URL'),
       CURLOPT_RETURNTRANSFER => true,
@@ -35,37 +35,37 @@ class CacheController extends Controller
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS => "{\"token\": \"". getenv('NGINX_CACHE_WARM_KEY') . "\",\"sitemap\": \"". getenv('SITE_URL') ."/sitemap.xml\"}",
+      CURLOPT_POSTFIELDS => "{\"token\": \"". getenv('NGINX_CACHE_WARM_KEY') . "\",\"sitemap\": \"". getenv('DEFAULT_SITE_URL') ."/sitemap.xml\"}",
       CURLOPT_HTTPHEADER => [
         "cache-control: no-cache",
         "content-type: application/json"
       ]
     ]);
-    
+
     $response = curl_exec($curl);
     $err = curl_error($curl);
-    
+
     curl_close($curl);
-    
-    if($err) 
+
+    if($err)
     {
       echo "cURL Error #:" . $err;
       die();
-    }   
-    
+    }
+
     $json = json_decode($response, true);
-    
+
     if(! $json['status'])
     {
       return $response;
       die();
     }
 
-    // Send notice the cache clear went well.    
+    // Send notice the cache clear went well.
     Craft::$app->getSession()->setNotice('Nginx Cache Cleared.');
-    
+
     return $this->redirect('centcom/cloudmanic-craft-nginx');
-  } 
+  }
 }
 
 /* End File */
